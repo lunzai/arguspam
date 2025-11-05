484da0a99476fac4bb1f7ee09bd04dfe2aed4cc5
HL <heanluen@gmail.com>
2025-11-05 18:33:55 +0800

    telescope

--- api/bootstrap/providers.php
+++ api/bootstrap/providers.php
@@ -4,6 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\HorizonServiceProvider::class,
     App\Providers\JitServiceProvider::class,
-    // Torann\GeoIP\GeoIPServiceProvider::class,
-    // Illuminate\Notifications\SlackChannelServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
 ];
